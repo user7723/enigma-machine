928f9da13e9cebd82dc385e1df0edf7c628e0eb6
user7723 <jmp.loop@kittens.org>
2023-02-16 16:31:55 +0300

    applicative parser for cli options

--- lib/Options/Parse.hs
+++ lib/Options/Parse.hs
@@ -1,90 +1,208 @@
 module Options.Parse
-  ( Option(..)
-  , parseOptions
+  ( ProgSpec(..)
+  , EnigmaSpec(..)
+  , EnigmaSpecOpt(..)
+  , EnigmaSpecFile(..)
+  , IOSpec(..)
+  , InputSpec(..)
+  , OutputSpec(..)
+  , parserOpts
+  , runArgs
   ) where
 
 import qualified Data.Text as T
 import qualified Data.Text.Read as T
+import Text.RawString.QQ
 
-import Data.List (stripPrefix)
-import Control.Applicative (Alternative(..))
-import Control.Monad (when)
-
-
+import Options.Applicative
+import Enigma.Constants (boundsInfo)
 import Enigma.Aliases
 
-data Option
-  = Help
-  | Bounds
-  | RoSerial (SerialNumber, SerialNumber, SerialNumber)
-  | ReSerial SerialNumber
-  | EState StateNumber
-  | Input FilePath
-  | Output FilePath
+runArgs :: IO ProgSpec
+runArgs = execParser parserOpts
+
+data ProgSpec = ProgSpec
+  { enigmaSpec :: EnigmaSpec
+  , ioSpec     :: IOSpec
+  } deriving Show
+
+parserOpts :: ParserInfo ProgSpec
+parserOpts =
+  info (progOptsp <**> helper)
+       (  progDesc desc
+       <> header hdr
+       <> briefDesc )
+  where
+    hdr = "enigma - the Enigma Machine Model"
+    desc = "Enigma is a command line program that (relatively inefficiently) encrypts your data either from a file or from `<stdin>`, and writes its output to a specified file or to `<stdout>`. It does not abide by the constraint of 26 symbols from the original hardware implementation, and currently it maps not the characters, but the bytes that represent them, so it's capable of encrypting text constituted with any set of characters."
+    progOptsp = parserBoundsInfo <*> parserProgSpec
+
+parserProgSpec :: Parser ProgSpec
+parserProgSpec
+   =  ProgSpec
+  <$> parserEnigmaSpec
+  <*> parserIOSpec
+
+boundsInfoHelp :: String
+boundsInfoHelp = "print max bounds of the enigma machine parameters, and other additional information"
+
+-- prints bounds-info and fails
+parserBoundsInfo :: Parser (ProgSpec -> ProgSpec)
+parserBoundsInfo
+  =  infoOption boundsInfo
+  (  long "bounds-info"
+  <> short 'b'
+  <> help boundsInfoHelp
+  )
+
+data EnigmaSpec
+  = EnigmaSpecO EnigmaSpecOpt
+  | EnigmaSpecF EnigmaSpecFile
+  | EnigmaSpecI
   deriving Show
 
-parseHelp :: String -> Maybe Option
-parseHelp s
-  = const Help
- <$> (stripPrefix "-h" s
- <|>  stripPrefix "--help" s)
+parserEnigmaSpec :: Parser EnigmaSpec
+parserEnigmaSpec = parserEnigmaSpecO <|> parserEnigmaSpecF <|> parserEnigmaSpecI
 
-parseBounds :: String -> Maybe Option
-parseBounds s
-  = const Bounds
- <$> (stripPrefix "-b" s
- <|>  stripPrefix "--bounds" s)
+parserEnigmaSpecO :: Parser EnigmaSpec
+parserEnigmaSpecO = EnigmaSpecO <$> parserEnigmaSpecOpt
 
-parseInputFile :: String -> Maybe Option
-parseInputFile s
-  = Input
- <$> (stripPrefix "-i" s
- <|>  stripPrefix "--input=" s)
+parserEnigmaSpecF :: Parser EnigmaSpec
+parserEnigmaSpecF = EnigmaSpecF <$> parserEnigmaSpecFile
 
-parseOutputFile :: String -> Maybe Option
-parseOutputFile s
-  =  Output
- <$> (stripPrefix "-o" s
- <|>  stripPrefix "--output=" s)
+parserEnigmaSpecI :: Parser EnigmaSpec
+parserEnigmaSpecI = pure EnigmaSpecI
 
-parseRoSerial :: String -> Maybe Option
-parseRoSerial o = do
-  s <- stripPrefix "-r" o <|> stripPrefix "--rotors=" o
-  let ts = T.split (== ',') $ T.pack s
-  -- lets just fix it to a magic number for now
-  when (length ts /= 3) empty
-  either
-    (const Nothing)
-    (Just . RoSerial . mkTriple . map fst)
-    $ traverse T.decimal ts
-  where
-    mkTriple (a:b:c:_) = (a,b,c)
-    mkTriple _ = error "you've probalby forgot about list size invariant"
+data EnigmaSpecOpt = EnigmaSpecOpt
+  { rotorNumbers    :: Rots
+  , reflectorNumber :: SerialNumber
+  , stateNumber     :: StateNumber
+  } deriving Show
 
-parseReSerial :: String -> Maybe Option
-parseReSerial o = do
-  s <- stripPrefix "-e" o <|> stripPrefix "--reflector=" o
-  either
-    (const Nothing)
-    (Just . ReSerial . fst)
-    $ T.decimal $ T.pack s
+parserEnigmaSpecOpt :: Parser EnigmaSpecOpt
+parserEnigmaSpecOpt
+   =  EnigmaSpecOpt
+  <$> parserRots
+  <*> parserReflector
+  <*> parserStateNumber
 
-parseEState :: String -> Maybe Option
-parseEState o = do
-  s <- stripPrefix "-s" o <|> stripPrefix "--state=" o
-  either
-    (const Nothing)
-    (Just . EState . fst)
-    $ T.decimal $ T.pack s
+readIntegral :: Integral a => ReadM a
+readIntegral = eitherReader (fmap fst . T.decimal . T.pack)
 
-parseOption :: String -> Maybe Option
-parseOption s =  parseHelp s
-             <|> parseBounds s
-             <|> parseRoSerial s
-             <|> parseReSerial s
-             <|> parseEState s
-             <|> parseInputFile s
-             <|> parseOutputFile s
+parserStateNumber :: Parser StateNumber
+parserStateNumber
+  = option readIntegral
+      (  long "init-state"
+      <> short 's'
+      <> metavar "<StateNumber>"
+      <> help "initial state of the Enigma Machine"
+      )
 
-parseOptions :: [String] -> Maybe [Option]
-parseOptions = traverse parseOption
+parserReflector :: Parser SerialNumber
+parserReflector
+  = option readIntegral
+      (  long "reflector"
+      <> short 'r'
+      <> metavar "<SerialNumber>"
+      <> help "reflectors serial number"
+      )
+
+data Rots = Rots
+  { rot1 :: SerialNumber
+  , rot2 :: SerialNumber
+  , rot3 :: SerialNumber
+  } deriving Show
+
+parserSerial :: String -> Char -> Parser SerialNumber
+parserSerial lon sho
+  = option readIntegral
+      (  long lon
+      <> short sho
+      <> metavar "<SerialNumber>"
+      <> help (lon ++ " serial number")
+      )
+
+parserRot1 :: Parser SerialNumber
+parserRot1 = parserSerial "first-rotor" '1'
+
+parserRot2 :: Parser SerialNumber
+parserRot2 = parserSerial "second-rotor" '2'
+
+parserRot3 :: Parser SerialNumber
+parserRot3 = parserSerial "third-rotor" '3'
+
+parserRots :: Parser Rots
+parserRots = Rots <$> parserRot1 <*> parserRot2 <*> parserRot3
+
+newtype EnigmaSpecFile = EnigmaSpecFile
+  { enigmaSpecFile :: FilePath
+  } deriving Show
+
+configHelp :: String
+configHelp = "ini file"
+
+parserEnigmaSpecFile :: Parser EnigmaSpecFile
+parserEnigmaSpecFile
+   =  EnigmaSpecFile
+  <$> strOption
+        (  long "config-file"
+        <> short 'c'
+        <> metavar "<ConfigFile>"
+        <> help configHelp
+        )
+
+data IOSpec = IOSpec
+  { inputSpec  :: InputSpec
+  , outputSpec :: OutputSpec
+  } deriving Show
+
+parserIOSpec :: Parser IOSpec
+parserIOSpec = IOSpec <$> parserInputSpec <*> parserOutputSpec
+
+data InputSpec
+  = InputStd
+  | InputFile FilePath
+  deriving Show
+
+inputHelp :: String
+inputHelp = "input file, default is <stdin>"
+
+parserInputStd :: Parser InputSpec
+parserInputStd = pure InputStd
+
+parserInputFile :: Parser InputSpec
+parserInputFile
+   =  InputFile
+  <$> strOption
+        (  long "input-file"
+        <> short 'i'
+        <> metavar "<InputFile>"
+        <> help inputHelp
+        )
+
+parserInputSpec :: Parser InputSpec
+parserInputSpec = parserInputFile <|> parserInputStd
+
+data OutputSpec
+  = OutputStd
+  | OutputFile FilePath
+  deriving Show
+
+outputHelp :: String
+outputHelp = "output file, default is <stdout>"
+
+parserOutputStd :: Parser OutputSpec
+parserOutputStd = pure OutputStd
+
+parserOutputFile :: Parser OutputSpec
+parserOutputFile
+   =  OutputFile
+  <$> strOption
+        (  long "output-file"
+        <> short 'o'
+        <> metavar "<OutputFile>"
+        <> help outputHelp
+        )
+
+parserOutputSpec :: Parser OutputSpec
+parserOutputSpec = parserOutputFile <|> parserOutputStd
